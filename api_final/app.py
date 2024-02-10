65c44c678aea5d5bbb0b101ec02df93e4d8a8981
robert <robertgleison12@gmail.com>
2024-02-10 06:56:00 +0000

    Última vez funcionando

--- api_final/app.py
+++ api_final/app.py
@@ -1,10 +1,16 @@
-from flask import render_template, Flask
-from csv_reader import get_csv_rows
-from server import app
-
-
+from flask import Flask, render_template
+import csv
 app = Flask(__name__)
 
+
+CSV_ROWS = []
+with open('Relatorio_cadop.csv', 'r', encoding='utf-8') as csvfile:
+    csvreader = csv.DictReader(csvfile, delimiter=';')
+    for row in csvreader:
+        CSV_ROWS.append(row)
+for i in CSV_ROWS:
+    print(i)
+#Separar controller de funções auxiliares
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -12,12 +18,15 @@ def index():
 @app.route('/search/<term>')
 def search(term):
     matching_rows = []
-    term_upper = term.upper()  
-    csv_rows = get_csv_rows()
-    for row in csv_rows:
+    term_upper = term.upper()  # Convert term to uppercase
+    for row in CSV_ROWS:
         for cell_value in row.values():
-            cell_value_upper = cell_value.upper()  
+            cell_value_upper = cell_value.upper()  # Convert cell_value to uppercase
             if term_upper in cell_value_upper:
                 matching_rows.append(row)
                 break
     return render_template('result-list.html', results=matching_rows)
+
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=9001)
